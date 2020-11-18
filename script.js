c7658ccd22050ec457454164f188b57d65c58c2b
Pontus Tahir <pontus.tahir@gmail.com>
2020-11-18 15:33:25 +0100

    a in btn

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const home = document.querySelector('.hem');
+const homeBase = document.querySelector('.hem');
 const about = document.querySelector('.about');
 const work = document.querySelector('.work');
 const contact = document.querySelector('.contact');
@@ -7,20 +7,20 @@ const contact = document.querySelector('.contact');
 
 function showAbout() {
     about.style.display = 'block'
-    home.style.display = 'none'
+    homeBase.style.display = 'none'
     work.style.display = 'none'
     contact.style.display = 'none'
 }
 function showContact() {
     contact.style.display = 'block'
     about.style.display = 'none'
-    home.style.display = 'none'
+    homeBase.style.display = 'none'
     work.style.display = 'none'
 }
 function showWork() {
     work.style.display = 'block'
     about.style.display = 'none'
-    home.style.display = 'none'
+    homeBase.style.display = 'none'
     contact.style.display = 'none'
 }
 
